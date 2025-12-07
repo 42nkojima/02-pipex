aeae14183e99aa32b683eeeab1624476c16140b6
nka21 <133028205+nka21@users.noreply.github.com>
2025-12-07 17:12:01 +0900

    wip

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: nkojima <nkojima@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 11:55:41 by nkojima           #+#    #+#             */
-/*   Updated: 2025/12/01 00:00:00 by nkojima          ###   ########.fr       */
+/*   Updated: 2025/12/06 16:47:24 by nkojima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,21 @@
 # include <string.h>
 # include "../libs/libft/libft.h"
 
-// 終了コード
 # define EXIT_SUCCESS 0
 # define EXIT_GENERAL_ERROR 1
 # define EXIT_CMD_NOT_FOUND 127
 # define EXIT_CMD_NOT_EXECUTABLE 126
 # define EXIT_SIGNAL_BASE 128
 
-// ファイルディスクリプタ
 # define FD_STDIN 0
 # define FD_STDOUT 1
 # define FD_STDERR 2
 
-// ファイルパーミッション
 # define FILE_PERMISSION 0644
 
-// システムコール戻り値
 # define SYSCALL_ERROR -1
 # define SYSCALL_SUCCESS 0
 
-// プログラム引数
 # define REQUIRED_ARGC 5
 
 // pipe.c
